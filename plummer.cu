902396d02b67f23ed085f2f1abd996487c30f159
Chen Lai <littlelittlecc@gmail.com>
2016-03-09 00:52:58 -0800

    first

---
+++ plummer.cu
@@ -0,0 +1,185 @@
+/*
+ * LEAPINT.C: program to integrate hamiltonian system using leapfrog.
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>  // rand(), srand()
+#include <time.h>       /* time */
+
+#define MAXPNT  100				/* maximum number of points */
+// #define GM 0.00029632889       /*4pi/365^2*/
+// #define Gmearth 8.8987655E-10
+// #define Gmsaturn 8.46797626E-8
+// #define Gmpartical 8.8987655E-11
+// #define PI 3.1415
+#define BLOCK_SIZE 256
+#define GM 1
+#define PI 3.14159265359
+#define NUM_PLANET 1024
+#define DT 0.1
+#define SOFTPARAMETER 0.00001
+
+
+void leapstep();				/* routine to take one step */
+
+void accel();					/* accel. for harmonic osc. */
+
+void printstate();				/* print out system state   */
+
+
+typedef struct Planet{          /*define a structure to store the position, velocity and dt for a planet*/
+  double* pos;
+  double* vel;
+} Planet;
+
+void initialize(Planet *planet)
+{
+    int i, j;
+    double x1, x2, x3, x4, x5, x6, x7;
+    double q;
+
+    srand (time(NULL));
+                                /* set number of planet  */
+
+/*Mercury*/
+    for (i = 0; i < NUM_PLANET; i++){
+        x1 = (double) rand() / (double)RAND_MAX;
+        x2 = (double)rand() / (double)RAND_MAX;					/* set initial position */
+        x3 = (double)rand() / (double)RAND_MAX;
+        x4 = (double)rand() / (double)RAND_MAX;
+        x5 = (double)rand() / (double)RAND_MAX;
+        x6 = (double)rand() / (double)RAND_MAX;
+        x7 = (double)rand() / (double)RAND_MAX;
+
+        planet[i].r =  pow( (pow(x1, (-2.0/3.0)) - 1), -0.5 );
+        planet[i].z =  (1.0 - 2.0*x2) * planet[i].r;
+        planet[i].x =  pow( planet[i].r*planet[i].r - planet[i].z*planet[i].z, 0.5 ) * cos(2.0 * PI * x3);
+        planet[i].y =  pow( planet[i].r*planet[i].r - planet[i].z*planet[i].z, 0.5 ) * sin(2.0 * PI * x3);
+
+
+        // while(0.1 * x5 >= ( x4 * x4 * pow((1 - x4 * x4), 3.5) )  ){
+        //     x4 = (double)rand() / (double)RAND_MAX;
+        //     x5 = (double)rand() / (double)RAND_MAX;
+        // }
+        //
+        // q = x4;
+        //
+        //
+        // planet[i].v =  q * pow(2.0, 0.5) * pow(1 + planet[i].r * planet[i].r, -0.25);					/* set initial position */
+        // planet[i].vz =  (1.0 - 2.0 * x6) * planet[i].v;
+        // planet[i].vx =  pow( planet[i].v * planet[i].v - planet[i].vz * planet[i].vz, 0.5) * cos(2.0 * PI * x7);
+        // planet[i].vy =  pow( planet[i].v * planet[i].v - planet[i].vz * planet[i].vz, 0.5) * sin(2.0 * PI * x7);
+
+        planet[i].vx =  0;
+        planet[i].vy =  0;
+        planet[i].vz =  0;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int i, mstep, nout, nstep;
+
+    mstep = 100;			/* number of steps to take  */
+    nout = 1;		    /* steps between outputs    */
+
+    /* first, set up initial conditions */
+
+    int bytes = 2*n*sizeof(double);
+
+    double *buf = (double*)malloc(bytes);
+    double *d_buf;
+    cudaMalloc(&d_buf, bytes);
+
+    Planet planet = (Planet){(double*)buf, ((double*)buf) + NUM_PLANET};
+    Planet d_planet = (Planet){(double*)d_buf, ((double*)d_buf) + NUM_PLANET};
+    const unsigned long nBlocks = (NUM_PLANET + BLOCK_SIZE - 1)/BLOCK_SIZE;
+
+    initialize(&planet);
+
+    cudaDeviceSetLimit(cudaLimitPrintfFifoSize, NUM_PLANET * 512);
+
+    cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
+
+    for (nstep = 0; nstep < mstep; nstep++){	/* loop mstep times in all  */
+	       if (nstep % nout == 0)			/* if time to output state  */
+	           printstate<<<nBlocks, BLOCK_SIZE>>>(d_planet.pos);		/* then call output routine */
+           cudaDeviceSynchronize();
+           accel<<<nBlocks, BLOCK_SIZE>>>(d_planet.pos, d_planet.vel);
+           cudaDeviceSynchronize();
+           leap_step<<<nBlocks, BLOCK_SIZE>>>(d_planet.pos, d_planet.vel);
+           cudaDeviceSynchronize();
+           accel<<<nBlocks, BLOCK_SIZE>>>(d_planet.pos, d_planet.vel);
+           cudaDeviceSynchronize();
+    }
+    if (mstep % nout == 0)			/* if last output wanted    */
+         printstate(planet);		/* then output last step    */
+
+    free(buf);
+    cudaFree(d_buf);
+}
+
+
+/*
+ * LEAPSTEP: take one step using the leap-from integrator, formulated
+ * as a mapping from t to t + dt.  WARNING: this integrator is not
+ * accurate unless the timestep dt is fixed from one call to another.
+ */
+__global__ void accel(double *pos, double *vel){
+    const unsigned long i = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned long tdx = threadIdx;
+
+    double ax = 0.0, ay = 0.0, az = 0.0;
+    double d_x = pos[i][0], d_y = pos[i][1], d_z = pos[i][0];
+    double norm;
+    int j, k;
+
+    __shared__ double sx[BLOCK_SIZE];
+    __shared__ double sy[BLOCK_SIZE];
+    __shared__ double sz[BLOCK_SIZE];
+
+    for(j = 0; j < gridDim; j++){
+        sx[tdx] = pos[j * BLOCK_SIZE + tdx][0];
+        sy[tdx] = pos[j * BLOCK_SIZE + tdx][1];
+        sz[tdz] = pos[j * BLOCK_SIZE + tdx][2];
+        __syncthreads();
+
+        for(k = 0; k < BLOCK_SIZE; k++){
+            norm = pow(SOFTPARAMETER + (d_x - sx[j]) * (d_x - sx[j]) + (d_y - sy[j]) * (d_y - sy[j]) + (d_z - sz[j]) * (d_z - sz[j]), 1.5 );
+            ax -= (1.0 * GM/NUM_PLANET) * (d_x - sx[j]) / norm;
+            ay -= (1.0 * GM/NUM_PLANET) * (d_y - sy[j]) / norm;
+            az -= (1.0 * GM/NUM_PLANET) * (d_z - sz[j]) / norm;
+        }
+        __syncthreads();
+    }
+
+    if(i < NUM_PLANET){
+        vel[i][0] += 0.5 * DT * ax;
+        vel[i][1] += 0.5 * DT * ay;
+        vel[i][2] += 0.5 * DT * az;
+    }
+}
+
+__global__ void leap_step(double *pos, double *vel){
+    const unsigned long i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i < NUM_PLANET){
+        pos[i][0] += DT * vel[i][0];
+        pos[i][1] += DT * vel[i][1];
+        pos[i][2] += DT * vel[i][2];
+    }
+}
+
+/*
+ * PRINTSTATE: output system state variables.
+ */
+
+__global__ void printstate(double *pos)					/* number of points         */
+{
+    const unsigned long i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < NUM_PLANET){		/* loop over all points...  */
+      	printf("%lu,%12.6f,%12.6f,%12.6f\n", i, planet[i].y,  planet[i].z);
+    }
+}
