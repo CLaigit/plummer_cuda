68d1d8f5c697c8d44f8a184603f8391648969cf9
Chen Lai <littlelittlecc@gmail.com>
2016-06-05 19:29:52 -0700

    test

--- isingmodel/3dising.sh
+++ isingmodel/3dising.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Tempurature=(1.9 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8)
-LATTICE_LENGTH=(30)
+Tempurature=(2.3)
+LATTICE_LENGTH=(256)
 # Input Tempurature you want to test, use space to separate different value
 
 for l in ${LATTICE_LENGTH[@]}; do
