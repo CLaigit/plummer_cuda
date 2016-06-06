66f1b5d45408ed4bfb67905780713234811217db
Chen Lai <chl588@igpu6-210.ucsd.edu>
2016-06-05 18:50:42 -0700

    n

--- isingmodel/2dising_cuda.sh
+++ isingmodel/2dising_cuda.sh
@@ -4,5 +4,5 @@ Tempurature=(2)
 # Input Tempurature you want to test, use space to separate different value
 for a in ${Tempurature[@]}; do
     echo Tempurature: $a    Outputfile: isingT$a.dat
-    ./ising2d.o $a  >   ising_cuda_T$a.dat
+    ./src/ising2d.o $a  >   data/ising_cuda_T$a.dat
 done
